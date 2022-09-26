10c222bf82a0a14116d01bf69c35b6821a171a64
Roman Radakhevych <akrir@ukr.net>
2022-09-26 18:13:22 +0300

    remove saving to localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { contactsReduser } from './contactsSlice';
-
-const persistConfig = {
-  key: 'phonebook',
-  storage,
-  whitelist: ['items'],
-};
-
-const contactsPersistedReducer = persistReducer(persistConfig, contactsReduser);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsPersistedReducer,
+    contacts: contactsReduser,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
